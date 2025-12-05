3c9abb4d4d9ed367e383a09041c23ff25ad29985
ZykoGit <micaiahejardin@gmail.com>
2025-12-04 21:24:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Welcome to Zforge - where the mods come alive!");
 });
+
+docuement.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("modUploadForm");
+  const status = document.getElementById("uploadStatus");
+
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      status.textContent = "Thanks for submitting your Upload! (Feature Coming Soon)"
+    });
+  }
+});
